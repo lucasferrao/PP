b836c9cc6715e075dc993f110fc0a94365aa2791
João Dias Pinheiro <49344877+jdiaspinheiro@users.noreply.github.com>
2019-05-14 17:14:57 +0100

    Add NewProject GUI and ManageUserProfile GUI

---
+++ HiProject/src/GUI/build/classes/CreateAccPage.rs
@@ -0,0 +1,5 @@
+GUI.src.CreateAccPage$4
+GUI.src.CreateAccPage$3
+GUI.src.CreateAccPage$2
+GUI.src.CreateAccPage$1
+GUI.src.CreateAccPage
---
+++ HiProject/src/GUI/build/classes/LogInPage.rs
@@ -0,0 +1,5 @@
+GUI.src.LogInPage
+GUI.src.LogInPage$4
+GUI.src.LogInPage$3
+GUI.src.LogInPage$2
+GUI.src.LogInPage$1
